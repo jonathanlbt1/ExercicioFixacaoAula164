31d70a6d297d3853de8ce102d693beb33a2ac85e
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2022-03-30 21:26:36 -0300

    Initial commit

---
+++ src/Exceptions/DomainException.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+import java.io.Serial;
+
+public class DomainException extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    public DomainException(String msg) {
+        super(msg);
+    }
+}
